d2aa74069187a186496cd842c79e46fc085fecee
David Ferrero <ferrero_d@hotmail.com>
2022-06-03 11:40:28 +0200

    WIP media

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,51 +1,48 @@
-const urlParams = new URLSearchParams(window.location.search); // on recherche dans l'url de la page
-const photographerLinkId = urlParams.get('id'); // on récupère l'Id du photographe dans l'url
+
 let photographers = [];
 let medias = [];
 
 // Récupération des données dans le fichier JSON
-async function getPhotographers() {
-    await fetch(`data/photographers.json`)
-        .then((res) => res.json())
-        .then((data) => {
-            photographers = data.photographers
-        });
+async function getPhotographers(data) {
+    const urlParams = new URLSearchParams(window.location.search); // on recherche dans l'url de la page
+    const photographerLinkId = urlParams.get('id'); // on récupère l'Id du photographe dans l'url
 
-    return photographers;
+    return new Photographer(data.find((p) => p.id == photographerLinkId));
 }
 
-async function getMedias() {
-    await fetch('data/photographers.json')
-        .then((res) => res.json())
-        .then((data) => (medias = data.media));
-
-    return medias;
+function displayProfileCard(photographer) {
+    const container = document.querySelector(".profile");
+    const userCard = photographer.createProfileCard();
+    container.appendChild(userCard);
 }
 
-getMedias();
-
-// Affichage de la card photographe
-async function cardPhotographer() {
-    const photographHeader = document.querySelector('.photograph-header');
-    await getPhotographers();
-
-    const cardP = photographers.find(
-        (photographer) => photographer.id == photographerLinkId,
-    );
-    const photographerDetails = cardFactory(cardP);
-    const getUserCard = photographerDetails.getUserCard();
-    photographHeader.appendChild(getUserCard);
+/* Gets the media corresponding to the photographer id */
+async function getMedia(data, photographerId) {
+    return data.filter((media) => media.photographerId == photographerId);
 }
-cardPhotographer();
 
-async function displayGallery() {
-    let gallery = await getPictures();
-    let logId;
+/* Displays the media card components */
+function displayMediaCard(media) {
+    media.forEach((medium) => {
+        const mediaData = new MediaFactory(medium, media);
+        const mediaCard = mediaData.createMediaCard();
+        mediaContainer.appendChild(mediaCard);
+    });
+}
 
-    const photographerGallery = document.querySelector('.portfolio');
-    const uniqGallery = galleryFactory(gallery);
-    const setPictures = uniqGallery.setPictures();
-    photographerGallery.appendChild(setPictures);
-    logId = gallery[0].photographerId;
-}  
-displayGallery()
+async function init() {
+    const { photographers, media } = await getPhotographers();
+    
+    const photographer = await getPhotographer(photographers);
+    displayProfileCard(photographer);
+    document.title = `${photographer.name} - Fisheye`;
+
+    const modalTitleName = document.getElementById("modal_titleName");
+    modalTitleName.textContent = photographer.name;
+
+    const photographerMedia = await getMedia(media, photographer.id);
+    sort(photographerMedia, sortByPopularity);
+    displayLikeCounter(photographerMedia, photographer.price);
+};
+
+init();
