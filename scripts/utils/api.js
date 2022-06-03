d8d97ab70a9d74b945510a0ffea076c48bbc0df3
David Ferrero <ferrero_d@hotmail.com>
2022-06-03 12:12:55 +0200

    mettre les fetch dans une api

--- scripts/utils/api.js
+++ scripts/utils/api.js
@@ -1,11 +1,26 @@
+let data;
+
+/* Chercher tous les photographers */
 async function getPhotographers() {
-    return await (await fetch("./data/photographers.json")).json();
-  }
-  async function init() {
-    const { photographers } = await getPhotographers();
-    displayData(photographers);
-    const { media } = await getPhotographers();
-    displayData(media);
-  }
-  
-  init();
+    if(data) {
+        console.log(data);
+        return data;
+    } else {
+        try {
+            let response = await fetch("data/photographers.json", {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "https://david-ferrero1.github.io/Front-End-Fisheye/",
+                }
+            });
+    
+            
+            data = await response.json();
+            return await data;
+        } catch(error) {
+            console.log(error);
+        }
+    }
+}
