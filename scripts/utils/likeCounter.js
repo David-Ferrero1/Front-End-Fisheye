66280f0316d2afc31929f561c81a8fa77671b3e4
David Ferrero <ferrero_d@hotmail.com>
2022-06-10 15:57:09 +0200

    create like counter

---
+++ scripts/utils/likeCounter.js
@@ -0,0 +1,7 @@
+function createLikeCounter(likes, price) {
+    const likeCounterContent = document.createElement("div");
+    likeCounterContent.setAttribute("class", "likeCounter-content");
+
+
+    return (likeCounterContent);
+}
