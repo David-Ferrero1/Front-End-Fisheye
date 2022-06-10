0b203073a2bc1ece9b060111780bb4c09ae80a2d
David Ferrero <ferrero_d@hotmail.com>
2022-06-10 15:57:44 +0200

    WIP create like counter

--- scripts/utils/likeCounter.js
+++ scripts/utils/likeCounter.js
@@ -2,6 +2,16 @@ function createLikeCounter(likes, price) {
     const likeCounterContent = document.createElement("div");
     likeCounterContent.setAttribute("class", "likeCounter-content");
 
+    const divLikes = document.createElement("div");
+    divLikes.setAttribute("class", "likeCounter_likes");
+
+    const likeAmount = document.createElement("p");
+    likeAmount.setAttribute("class", "likeCounter_amount");
+    likeAmount.textContent = likes;
+
+    
+
+
 
     return (likeCounterContent);
 }
